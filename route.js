f6372b071d1bb5ab7fb263659cd2bcc441e1cfd2
Riky Fahri Hasibuan <riky.hasibuan@gmail.com>
2021-01-17 00:59:24 +0700

    add parameter for route

--- route.js
+++ route.js
@@ -4,5 +4,5 @@ module.exports = function(app) {
     let route = require('./controller')
     app.route('/').get(route.index)
     app.route('/stock/:symbol').get(route.stock)
-    app.route('/stocks').get(route.stocks)
+    app.route('/stocks/:resolution').get(route.stocks)
 };
